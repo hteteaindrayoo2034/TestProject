9ff6763be1a1362217f02c967faec3b148908320
hteteaindrayoo2034 <hteteaindrayoo30@gmail.com>
2025-05-06 14:15:00 +0630

    First Commit

---
+++ src/main/java/com/testprojects/Main.java
@@ -0,0 +1,46 @@
+package com.testprojects;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    private Connection get_Db_Connection(){
+        Connection con = null;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb", "root", "");
+            System.out.println("Database is connected.");
+        }catch (ClassNotFoundException ce){
+            System.out.println(ce.getMessage());
+        }
+        catch (SQLException se) {
+            se.printStackTrace();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return con;
+    }
+
+
+    public static void main(String[] args) {
+
+        Main m = new Main();
+        Connection con = m.get_Db_Connection();
+        try{
+            con.close();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        // connection
+        // statement
+        // Resultset 1 ka ny sa tr
+        // close
+
+
+
+        }
+    }
